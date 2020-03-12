0d0ecf9b4906ea473aefa8a445957d03549bebfb
Mike Ludemann <mike-ludemann@hotmail.de>
2020-03-12 15:32:15 +0100

    Setup complete - vue-dynamic-table

---
+++ src/components/dynamic-table.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+
+	<section class="dynamic-table">
+		<table id='contact'>
+			<tbody id="contentData">
+				<tr>
+					<th v-for="(key, value) in Object.keys(this.datas[0])" :key="value">{{key}}</th>
+				</tr>
+				<tr v-for="data in datas" :key="id">
+					<td>{{data.id}}</td>
+					<td>{{data.name}}</td>
+					<td>{{data.age}}</td>
+					<td>{{data.email}}</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'DynamicTable',
+		props: {
+			datas: {
+				type: Array
+			}
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+
+		}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+.dynamic-table {
+
+}
+
+#contact {
+	text-align: center;
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	border-collapse: collapse;
+	border: 3px solid #ddd;
+	width: 100%;
+}
+
+#contact td, #contact th {
+	border: 1px solid #ddd;
+	padding: 8px;
+}
+
+#contact tr:nth-child(even){background-color: #f2f2f2;}
+
+#contact tr:hover {background-color: #ddd;}
+
+#contact th {
+	padding-top: 12px;
+	padding-bottom: 12px;
+	text-align: center;
+	background-color: #15d81b;
+	color: white;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DynamicTable :datas="[{ id: 1, name: 'John Doe', age: 21, email: 'john.doe@email.com' }, { id: 2, name: 'Mary Doe', age: 19, email: 'mary.doe@email.com' }, { id: 3, name: 'Jim Doe', age: 16, email: 'jim.doe@email.com' }, { id: 4, name: 'Martha Doe', age: 25, email: 'martha.doe@email.com' }]"></DynamicTable>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import DynamicTable from '@/components/dynamic-table.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    DynamicTable
+  }
+}
+</script>
